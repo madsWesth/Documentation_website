f018df88695360dd36b9da39acdee1b6da83f3bd
madsWesth <72575386+madsWesth@users.noreply.github.com>
2022-03-07 23:57:08 +0100

    added content to the pages and restructure

--- app.js
+++ app.js
@@ -10,23 +10,19 @@ const header = fs.readFileSync("./public/components/header-nav.html").toString()
 const footer = fs.readFileSync("./public/components/footer.html").toString()
 
 //content
-const environmentPage               = fs.readFileSync("./public/pages/environments/environments.html").toString()
 const frontpage                     = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString()
 const javascripConceptsPage         = fs.readFileSync("./public/pages/javascript_concepts/javascript_concepts.html").toString()
 const nodePage                      = fs.readFileSync("./public/pages/node/node.html").toString()
 const projectPackageManagerPage     = fs.readFileSync("./public/pages/project_package_manager/project_package_manager.html").toString()
 const restPage                      = fs.readFileSync("./public/pages/rest/rest.html").toString()
-const ssrPage                       = fs.readFileSync("./public/pages/ssr/ssr.html").toString()
 const strategiesPage                = fs.readFileSync("./public/pages/strategies/strategies.html").toString()
 
 //constructed webpages
-const constructedEnvironmentPage             = header.replace("%%TITLE_PLACEHOLDER", "Environment").replace("%%ENVIRONMENTS_ACTIVE", "active") + environmentPage + footer
 const constructedFrontpage                   = header.replace("%%TITLE_PLACEHOLDER", "Home").replace("%%HOME_ACTIVE", "active") + frontpage + footer
 const constructedJavascriptConceptsPage      = header.replace("%%TITLE_PLACEHOLDER", "Javascript concepts").replace("%%JS_CONCEPTS_ACTIVE", "active") + javascripConceptsPage + footer
 const constructedNodePage                    = header.replace("%%TITLE_PLACEHOLDER", "Node").replace("%%NODE_ACTIVE", "active") + nodePage + footer
 const constructedProjectPackageManagerPage   = header.replace("%%TITLE_PLACEHOLDER", "Project-/Package Manager").replace("%%PROJECT_ACTIVE", "active") + projectPackageManagerPage + footer
 const constructedRestPage                    = header.replace("%%TITLE_PLACEHOLDER", "REST and Express").replace("%%REST_ACTIVE", "active") + restPage + footer
-const constructedSsrPage                     = header.replace("%%TITLE_PLACEHOLDER", "Server Side Rendering").replace("%%SSR_ACTIVE", "active") + ssrPage + footer
 const constructedStrategiesPage              = header.replace("%%TITLE_PLACEHOLDER", "Strategies and issues serving content").replace("%%STRATEGIES_ACTIVE", "active") + strategiesPage + footer
 
 app.use(express.static("public"))
@@ -65,7 +61,7 @@ app.get("/SSR", (req, res) => {
 
 
 
-//fallback port in none is setup as an env variable
+//fallback port
 const PORT = process.env.PORT || 9000
 
 app.listen(PORT, (req, res) => {
